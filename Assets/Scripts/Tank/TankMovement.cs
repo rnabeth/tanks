89bbc3edbd1eaba975a58581d69b2bd0defa5fd9
Rafael Nabeth <rafaelnabeth@gmail.com>
2015-11-04 00:07:47 +0000

    Tank movement

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -2,42 +2,39 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;         
-    public float m_Speed = 12f;            
-    public float m_TurnSpeed = 180f;       
-    public AudioSource m_MovementAudio;    
-    public AudioClip m_EngineIdling;       
-    public AudioClip m_EngineDriving;      
+    public int m_PlayerNumber = 1;
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+    public AudioSource m_MovementAudio;
+    public AudioClip m_EngineIdling;
+    public AudioClip m_EngineDriving;
     public float m_PitchRange = 0.2f;
 
-    /*
-    private string m_MovementAxisName;     
-    private string m_TurnAxisName;         
-    private Rigidbody m_Rigidbody;         
-    private float m_MovementInputValue;    
-    private float m_TurnInputValue;        
-    private float m_OriginalPitch;         
-
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
+    private float m_OriginalPitch;
 
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-
-    private void OnEnable ()
+    //Called after Awake but before any of the Updates
+    private void OnEnable()
     {
         m_Rigidbody.isKinematic = false;
+        //So no forces are applied, for instance when the tank is hit by the enemy shell
         m_MovementInputValue = 0f;
         m_TurnInputValue = 0f;
     }
 
-
-    private void OnDisable ()
+    private void OnDisable()
     {
         m_Rigidbody.isKinematic = true;
     }
-
 
     private void Start()
     {
@@ -46,34 +43,68 @@ public class TankMovement : MonoBehaviour
 
         m_OriginalPitch = m_MovementAudio.pitch;
     }
-    */
 
+    //Runs every frame
     private void Update()
     {
         // Store the player's input and make sure the audio for the engine is playing.
-    }
 
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+        EngineAudio();
+    }
 
     private void EngineAudio()
     {
         // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
+        {
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+        else
+        {
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
     }
 
-
+    //Runs every physics step
     private void FixedUpdate()
     {
         // Move and turn the tank.
-    }
 
+        Move();
+        Turn();
+    }
 
     private void Move()
     {
         // Adjust the position of the tank based on the player's input.
-    }
 
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime; //Time.deltaTime -> To smooth it per frame, move per second
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
 
     private void Turn()
     {
         // Adjust the rotation of the tank based on the player's input.
+
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f); //Quaternion is used to store rotation
+
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
     }
 }
