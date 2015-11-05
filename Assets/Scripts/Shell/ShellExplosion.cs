b2addd8fad84c8fb4cfa60f216eab2160f6f0677
Rafael Nabeth <rafaelnabeth@gmail.com>
2015-11-05 22:41:03 +0000

    Shell Creation

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -3,29 +3,64 @@
 public class ShellExplosion : MonoBehaviour
 {
     public LayerMask m_TankMask;
-    public ParticleSystem m_ExplosionParticles;       
-    public AudioSource m_ExplosionAudio;              
-    public float m_MaxDamage = 100f;                  
-    public float m_ExplosionForce = 1000f;            
-    public float m_MaxLifeTime = 2f;                  
-    public float m_ExplosionRadius = 5f;              
-
+    public ParticleSystem m_ExplosionParticles;
+    public AudioSource m_ExplosionAudio;
+    public float m_MaxDamage = 100f;
+    public float m_ExplosionForce = 1000f;
+    public float m_MaxLifeTime = 2f;
+    public float m_ExplosionRadius = 5f;
 
     private void Start()
     {
-        Destroy(gameObject, m_MaxLifeTime);
+        Destroy(gameObject, m_MaxLifeTime); //When instantiating this, if it's still alive after 2 seconds then destroy it
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
-    }
 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask); //TankMask used to only pick up tanks
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody) //Sanity check
+                continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            if (!targetHealth) //Sanity check
+                continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            targetHealth.TakeDamage(damage);
+        }
+
+        m_ExplosionParticles.transform.parent = null; //So it still plays after the shell is destroyed
+
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration); //We want to destroy the game object, not the component (m_ExplosionParticles)
+        Destroy(gameObject);
+    }
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+        float damage = relativeDistance * m_MaxDamage;
+
+        damage = Mathf.Max(0f, damage); //Because the tank can be outside the edge of the explosion circle but its collider might be inside
+
+        return damage;
     }
 }
