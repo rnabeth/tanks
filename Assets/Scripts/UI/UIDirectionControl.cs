3dc17bc2c54063020320ebc4c132762c599777ef
Rafael Nabeth <rafaelnabeth@gmail.com>
2015-11-05 00:01:49 +0000

    Tank Health

--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -2,21 +2,18 @@ using UnityEngine;
 
 public class UIDirectionControl : MonoBehaviour
 {
-    public bool m_UseRelativeRotation = true;  
+    public bool m_UseRelativeRotation = true;
 
-
-    private Quaternion m_RelativeRotation;     
-
+    private Quaternion m_RelativeRotation;
 
     private void Start()
     {
-        m_RelativeRotation = transform.parent.localRotation;
+        m_RelativeRotation = transform.parent.localRotation; //Find the local rotation of the canvas
     }
-
 
     private void Update()
     {
         if (m_UseRelativeRotation)
-            transform.rotation = m_RelativeRotation;
+            transform.rotation = m_RelativeRotation; //To keep the rotation static
     }
 }
